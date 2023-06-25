341058c08944f34726f5bd946d7cbc0f17fc46ca
Aleksandr Savkin <voyager5874@gmail.com>
2023-06-25 20:57:08 +0300

    hw 14

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import HW15 from "s2-homeworks/hw15/HW15";
+import HW14 from "s2-homeworks/hw14/HW14";
 // import HW10 from '../../hw10/HW10'
 // import HW11 from '../../hw11/HW11'
 // import HW12 from '../../hw12/HW12'
 // import HW13 from '../../hw13/HW13'
-// import HW14 from '../../hw14/HW14'
 // import HW15 from '../../hw15/HW15'
 
 function JuniorPlus() {
@@ -14,7 +14,7 @@ function JuniorPlus() {
       {/*<HW11 />*/}
       {/*<HW12 />*/}
       {/*<HW13 />*/}
-      {/*<HW14 />*/}
+      <HW14 />
       <HW15 />
     </div>
   );
